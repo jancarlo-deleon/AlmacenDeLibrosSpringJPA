6ed90ce390d297a76e2cc3a418dc9511f55e6b81
Jan Carlo De León <janleoncif11@gmail.com>
2024-06-24 12:10:56 -0600

    first commit

---
+++ src/main/java/com/jdeleon/challengeliteratura/ChallengeliteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.jdeleon.challengeliteratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeliteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeliteraturaApplication.class, args);
+	}
+
+}
